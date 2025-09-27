ebfdc1137b960023cce1345608e3a4ab1e62a3d5
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-27 07:14:46 -0500

    Improve MongoDB connection handling

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/agile-task-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(error => {
+  console.error('MongoDB connection error:', error);
 });
 
 app.get('/', (req, res) => {
